209ce45a43e2fa31704deaac189efcabe1cfee45
Josh Wolfe <joshuafwolfe@gmail.com>
2016-11-10 00:25:29 -0500

    Added stack

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "stack.c"
+
+#define left -99
+#define right -98
+#define up -97
+#define down -96
+
+Stack *stk;
+int current_direction = left;
+int crow = 0, ccol = 0;
 
 char* ptr_to_xy(char *list, int row, int col) {
   char *ptr = list;
@@ -11,8 +21,39 @@ char* ptr_to_xy(char *list, int row, int col) {
   return (ptr + col);
 }
 
+void move(char *list, char *current_post) {
+  if (current_direction == left) {
+  }
+
 char* process_char(char *list, char *current_pos) {
   printf("%c\n", *current_pos);
+
+  if (*current_pos == '+') {
+    int var1, var2;
+    var1 = Stack_pop(stk);
+    var2 = Stack_pop(stk);
+    Stack_push(stk, (var1 + var2));
+  } else if (*current_pos == '-') {
+    int var1, var2;
+    var1 = Stack_pop(stk);
+    var2 = Stack_pop(stk);
+    Stack_push(stk, (var1 - var2));
+  } else if (*current_pos == '*') {
+    int var1, var2;
+    var1 = Stack_pop(stk);
+    var2 = Stack_pop(stk);
+    Stack_push(stk, (var1 * var2));
+  } else if (*current_pos == '/') {
+    int var1, var2;
+    var1 = Stack_pop(stk);
+    var2 = Stack_pop(stk);
+    Stack_push(stk, (var2 / var1));
+  }
+
+
+  else if (*current_pos == '$') {
+    Stack_pop(stk);
+  }
   return current_pos;
 }
 
@@ -23,6 +64,7 @@ int main(int argc, char **argv) {
   FILE *fd;
   int size;
   char *list, *itr;
+  stk = malloc(sizeof(stk));
 
   if ((fd = fopen(argv[1], "r")) == NULL) {
     perror(argv[1]);
@@ -48,12 +90,9 @@ int main(int argc, char **argv) {
   fclose(fd);
   free(line);
 
-  printf("%s\n", list);
-
   char *current_pos = ptr_to_xy(list, 0, 0);
-
   while (1) {
-    current_pos = process_char(list, current_pos);
+    current_pos = process_char(list, current_pos, crow, ccol);
   }
 
   return 0;
---
+++ stack.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX_SIZE 100
+
+typedef struct Stack {
+  int top;
+  int data[MAX_SIZE];
+} Stack;
+
+int size(Stack *s) {
+  return s->top;
+}
+
+int Stack_empty(Stack *s) {
+  if (size(s) < 0)
+    return 0;
+  else
+    return 1;
+}
+
+int Stack_full(Stack *s) {
+  if (size(s) == MAX_SIZE - 1)
+    return 0;
+  else
+    return 1;
+}
+
+int Stack_push(Stack *s, int new) {
+  if (Stack_full(s) == 0)
+    return -1;
+  else {
+    s->top++;
+    s->data[s->top] = new;
+  }
+  return 0;
+}
+
+int Stack_pop(Stack *s) {
+  if (Stack_empty(s) == 0)
+    return -1;
+  else {
+    int val = s->data[s->top];
+    s->top--;
+    return val;
+  }
+}
+
+int Stack_peek(Stack *s) {
+  if (Stack_empty(s) == 0)
+    return -1;
+  else
+    return s->data[s->top];
+}
+
+// For testing
+//int main(int argc, char **argv) {
+//Stack *stk = malloc(sizeof(stk));
+//
+//Stack_push(stk, 7);
+//printf("%d\n", Stack_peek(stk));
+//printf("%d\n", Stack_peek(stk));
+//
+//return 0;
+//}
